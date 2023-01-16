e99c469b4b92c1db684d7fc7928d1124e354f9e1
zacisyourmaster <zac15590@gmail.com>
2023-01-15 19:32:21 -0800

    Merge branch 'main' of

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ const startButton = document.querySelector('.start');
 const stopButton = document.querySelector('.stop');
 const resetButton = document.querySelector('.reset');
 const shortBreak = document.querySelector('.short-break');
+const longBreak = document.querySelector('.long-break');
+const cycles = document.querySelector('.cycle');
 let timerInterval;
 let timerTime = 1500;
-let defTime = timerTime;
 let totalTime = 0;
+let nCycles = 0;
 function startTimer() {
   timerInterval = setInterval(() => {
     timerTime--;
@@ -18,7 +20,7 @@ function stopTimer() {
 }
 function resetTimer(){
     clearInterval(timerInterval)
-    timerTime=defTime;
+    timerTime=1500;
     displayTime();
 }
 function displayTime() {
@@ -28,11 +30,34 @@ function displayTime() {
       seconds = `0${seconds}`;
     }
     timerDisplay.textContent = `${minutes}:${seconds}`;
+    if(seconds==00 && minutes == 0){
+      nCycles++;
+      cycles.textContent= `You've Completed ${nCycles} Cycles`
+    }
+    if(nCycles>0){
+      sBreak();
+    }
+}
+function sBreak(){
+  timerTime=300;
+  clearInterval(timerInterval);
+  displayTime();
+}
+function lBreak(){
+  clearInterval(timerInterval);
+timerTime=900;
+displayTime();
 }
 startButton.addEventListener('click', startTimer);
 stopButton.addEventListener('click', stopTimer);
 resetButton.addEventListener('click', resetTimer);
-/*shortBreak.addEventListener('click', function(){
-  timerTime=300;
-  resetTimer();
-});*/
+shortBreak.addEventListener('click', () => {
+  timerTime = 300; // 5 minutes in seconds
+  clearInterval(timerInterval);
+  displayTime();
+});
+longBreak.addEventListener('click', () => {
+  timerTime = 900; // 15 minutes in seconds
+  clearInterval(timerInterval);
+  displayTime();
+});
