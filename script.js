5ad1d61f1945f34127ea52596052179af046c5b7
zacisyourmaster <112670877+zacisyourmaster@users.noreply.github.com>
2023-11-08 10:40:44 -0800

    Update script.js
    made it so title changes

--- script.js
+++ script.js
@@ -9,53 +9,66 @@ let timerInterval;
 let timerTime = 1500;
 let totalTime = 0;
 let nCycles = 0;
+
 function startTimer() {
   timerInterval = setInterval(() => {
     timerTime--;
     displayTime();
   }, 1000);
 }
+
 function stopTimer() {
   clearInterval(timerInterval);
 }
-function resetTimer(){
-    clearInterval(timerInterval)
-    timerTime=1500;
-    displayTime();
+
+function resetTimer() {
+  clearInterval(timerInterval);
+  timerTime = 1500;
+  displayTime();
 }
+
 function displayTime() {
-    let minutes = Math.floor(timerTime / 60);
-    let seconds = timerTime % 60;
-    if (seconds < 10) {
-      seconds = `0${seconds}`;
-    }
-    timerDisplay.textContent = `${minutes}:${seconds}`;
-    if(seconds==00 && minutes == 0){
-      nCycles++;
-      cycles.textContent= `You've Completed ${nCycles} Cycles`
-    }
-    if(nCycles>0){
-      sBreak();
-    }
+  let minutes = Math.floor(timerTime / 60);
+  let seconds = timerTime % 60;
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+  timerDisplay.textContent = `${minutes}:${seconds}`;
+  
+  // Set the document title with the current timer value
+  document.title = `${minutes}:${seconds} - Pomodoro App`;
+  
+  if (seconds == 0 && minutes == 0) {
+    nCycles++;
+    cycles.textContent = `You've Completed ${nCycles} Cycles`;
+  }
+  if (nCycles > 0) {
+    sBreak();
+  }
 }
-function sBreak(){
-  timerTime=300;
+
+function sBreak() {
+  timerTime = 300;
   clearInterval(timerInterval);
   displayTime();
 }
-function lBreak(){
+
+function lBreak() {
   clearInterval(timerInterval);
-timerTime=900;
-displayTime();
+  timerTime = 900;
+  displayTime();
 }
+
 startButton.addEventListener('click', startTimer);
 stopButton.addEventListener('click', stopTimer);
 resetButton.addEventListener('click', resetTimer);
+
 shortBreak.addEventListener('click', () => {
   timerTime = 300; // 5 minutes in seconds
   clearInterval(timerInterval);
   displayTime();
 });
+
 longBreak.addEventListener('click', () => {
   timerTime = 900; // 15 minutes in seconds
   clearInterval(timerInterval);
