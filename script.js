6749d881f4fd0b89241efe5ae695b730872cc651
zacisyourmaster <zac15590@gmail.com>
2025-06-17 13:35:49 -0700

    small update. going to refractor app later

--- script.js
+++ script.js
@@ -1,76 +1,75 @@
-const timerDisplay = document.querySelector('.timer-display');
-const startButton = document.querySelector('.start');
-const stopButton = document.querySelector('.stop');
-const resetButton = document.querySelector('.reset');
-const shortBreak = document.querySelector('.short-break');
-const longBreak = document.querySelector('.long-break');
-const cycles = document.querySelector('.cycle');
-let timerInterval;
+const timerDisplay = document.querySelector(".timer-display");
+const startButton = document.querySelector(".start");
+const stopButton = document.querySelector(".stop");
+const resetButton = document.querySelector(".reset");
+const shortBreak = document.querySelector(".short-break");
+const longBreak = document.querySelector(".long-break");
+const focus = document.querySelector(".focus");
+const cycles = document.querySelector(".cycle");
+const timerInterval = () => {
+    setInterval(() => {
+        timerTime--;
+        displayTime();
+    }, 1000);
+};
 let timerTime = 1500;
 let totalTime = 0;
 let nCycles = 0;
 
-function startTimer() {
-  timerInterval = setInterval(() => {
-    timerTime--;
-    displayTime();
-  }, 1000);
-}
-
-function stopTimer() {
-  clearInterval(timerInterval);
-}
-
-function resetTimer() {
-  clearInterval(timerInterval);
-  timerTime = 1500;
-  displayTime();
-}
-
 function displayTime() {
-  let minutes = Math.floor(timerTime / 60);
-  let seconds = timerTime % 60;
-  if (seconds < 10) {
-    seconds = `0${seconds}`;
-  }
-  timerDisplay.textContent = `${minutes}:${seconds}`;
-  
-  // Set the document title with the current timer value
-  document.title = `${minutes}:${seconds} - Pomodoro App`;
-  
-  if (seconds == 0 && minutes == 0) {
-    nCycles++;
-    cycles.textContent = `You've Completed ${nCycles} Cycles`;
-  }
-  if (nCycles > 0) {
-    sBreak();
-  }
+    let minutes = Math.floor(timerTime / 60);
+    let seconds = timerTime % 60;
+    if (seconds < 10) {
+        seconds = `0${seconds}`;
+    }
+    timerDisplay.textContent = `${minutes}:${seconds}`;
+
+    // Set the document title with the current timer value
+    document.title = `${minutes}:${seconds} - Pomodoro App`;
+
+    if (seconds == 0 && minutes == 0) {
+        nCycles++;
+        cycles.textContent = `You've Completed ${nCycles} Cycles`;
+        clearInterval(timerInterval);
+    }
+    // if (nCycles > 0) {
+    //     sBreak();
+    // }
 }
 
-function sBreak() {
-  timerTime = 300;
-  clearInterval(timerInterval);
-  displayTime();
-}
+document.addEventListener("DOMContentLoaded", () => {
+    startButton.addEventListener("click", () => {
+        timerInterval = setInterval(() => {
+            timerTime--;
+            displayTime();
+        }, 1000);
+    });
 
-function lBreak() {
-  clearInterval(timerInterval);
-  timerTime = 900;
-  displayTime();
-}
+    stopButton.addEventListener("click", () => clearInterval(timerInterval));
 
-startButton.addEventListener('click', startTimer);
-stopButton.addEventListener('click', stopTimer);
-resetButton.addEventListener('click', resetTimer);
+    resetButton.addEventListener("click", () => {
+        {
+            clearInterval(timerInterval);
+            timerTime = 1500;
+            displayTime();
+        }
+    });
 
-shortBreak.addEventListener('click', () => {
-  timerTime = 300; // 5 minutes in seconds
-  clearInterval(timerInterval);
-  displayTime();
-});
+    shortBreak.addEventListener("click", () => {
+        timerTime = 300; // 5 minutes in seconds
+        clearInterval(timerInterval);
+        displayTime();
+    });
 
-longBreak.addEventListener('click', () => {
-  timerTime = 900; // 15 minutes in seconds
-  clearInterval(timerInterval);
-  displayTime();
+    longBreak.addEventListener("click", () => {
+        timerTime = 900; // 15 minutes in seconds
+        clearInterval(timerInterval);
+        displayTime();
+    });
+
+    focus.addEventListener("click", () => {
+        timerTime = 1500; // 25 minutes in seconds
+        clearInterval(timerInterval);
+        displayTime();
+    });
 });
